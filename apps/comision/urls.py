65fd67f78dfa153bc7adfcf47f3eaee7a29d7123
sink0la <ferreyra006@gmail.com>
2023-11-13 16:24:25 -0300

    uwusosa

--- apps/comision/urls.py
+++ apps/comision/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from apps.comision.views import MiembrosTE_List, MiembrosTE_Create
+from apps.comision.views import MiembrosTE_List, MiembrosTE_Create, MiembrosTE_Detail, MiembrosTE_Eliminar, MiembrosTE_Editar
 
 app_name = 'comision'
 
 urlpatterns = [
     path('listarte/', MiembrosTE_List.as_view(), name='miembroTE_list'),
     path('miembrote/nuevo/', MiembrosTE_Create.as_view(), name='miembroTE_crear'),
-    #path('miembrote/<int:pk>/', views.miembro_te_detail, name='miembrote_detail'),
-    #path('miembrote_delete/<int:pk>/', views.miembro_te_delete, name='miembrote_delete'),
-    #path('miembrote_edit/<int:pk>/', views.miembro_te_edit, name='miembrote_edit'),
+    path('miembrote/<int:pk>/', MiembrosTE_Detail.as_view(), name='miembroTE_detalle'),
+    path('miembrote_delete/<int:pk>/', MiembrosTE_Eliminar.as_view(), name='miembroTE_eliminar'),
+    path('miembrote_edit/<int:pk>/', MiembrosTE_Editar.as_view(), name='miembroTE_editar'),
 
 ]
