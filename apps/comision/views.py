a24e483261b8f2a0039793f6c9040f8b0ec08d60
Fantitom03 <leivafrancisco2017@gmail.com>
2023-11-13 16:26:25 -0300

    Squashed commit of the following:
    commit 65fd67f78dfa153bc7adfcf47f3eaee7a29d7123
    Author: sink0la <ferreyra006@gmail.com>
    Date:   Mon Nov 13 16:24:25 2023 -0300
    
        uwusosa
    
    commit 757ff1a415df27f246709a1de51773d28b03a6d4
    Author: sink0la <ferreyra006@gmail.com>
    Date:   Sun Nov 12 18:24:32 2023 -0300
    
        ejemplo listar y crear con views
    
    commit 5e340dfb978153a8f9d9039c0b1783fa888935dc
    Author: sink0la <ferreyra006@gmail.com>
    Date:   Sun Nov 12 16:57:01 2023 -0300
    
        creando views con clases

--- apps/comision/views.py
+++ apps/comision/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView
 from django.urls import reverse_lazy
 
 from apps.comision.forms import MiembroTE_Form,  Miembro_CSTF_Form, TribunalEvaluadorForm
@@ -14,3 +14,32 @@ class MiembrosTE_Create(CreateView):
     form_class = MiembroTE_Form
     template_name = 'TE/miembroTE_crear.html'
     success_url = reverse_lazy('comision:miembroTE_list')
+
+class MiembrosTE_Detail(DetailView):
+    model = Miembro_TE
+    template_name = 'TE/miembroTE_detalle.html'
+
+
+class MiembrosTE_Eliminar(DeleteView):
+    model = Miembro_TE
+    success_url = reverse_lazy('comision:miembroTE_list')
+
+
+class MiembrosTE_Editar(UpdateView):
+    model = Miembro_TE
+    form_class = MiembroTE_Form
+    template_name = 'TE/miembroTE_crear.html'
+    success_url = reverse_lazy('comision:miembroTE_list')
+
+
+
+
+
+
+
+
+
+
+
+
+
