b3fa589c24298b630ff007a7b8d0620ea2804eac
sink0la <ferreyra006@gmail.com>
2023-11-17 02:14:01 -0300

    Squashed commit of the following:
    commit 7ac6cd6164f101f39a24bdd12570a1f66d2b6786
    Author: agustinhussein1 <agustinhusseindiaz@gmail.com>
    Date:   Fri Nov 17 00:51:19 2023 -0300
    
        rama hussein
    
    commit 24179aaaf4cd54e709a9cd8ca1eb8d4afa518263
    Merge: c865215 af94e83
    Author: agustinhussein1 <agustinhusseindiaz@gmail.com>
    Date:   Thu Nov 16 22:01:11 2023 -0300
    
        Merge branch 'Fantom' into hussein
    
    commit c86521549f9b7de747a6acabc2a003406b786a3f
    Author: agustinhussein1 <agustinhusseindiaz@gmail.com>
    Date:   Wed Nov 15 17:30:39 2023 -0300
    
        commit by fantom
    
    commit af94e834997ad5b39b7ca3ed9ad80ae9247c9ade
    Author: Fantitom03 <leivafrancisco2017@gmail.com>
    Date:   Wed Nov 15 15:45:11 2023 -0300
    
        Squashed commit of the following:
    
        commit 91962640954b25421bcfa57ccff712ae6def17e5
        Author: agustinhussein1 <agustinhusseindiaz@gmail.com>
        Date:   Mon Nov 13 20:17:36 2023 -0300
    
            (sigue siendo mi rama)
    
        commit 2f0b3407c3755a66e34fb0ec035907337ca11406
        Author: agustinhussein1 <agustinhusseindiaz@gmail.com>
        Date:   Mon Nov 13 20:17:17 2023 -0300
    
            cambios rama hussein (es mi rama)
    
        commit 65fd67f78dfa153bc7adfcf47f3eaee7a29d7123
        Author: sink0la <ferreyra006@gmail.com>
        Date:   Mon Nov 13 16:24:25 2023 -0300
    
            uwusosa
    
        commit 757ff1a415df27f246709a1de51773d28b03a6d4
        Author: sink0la <ferreyra006@gmail.com>
        Date:   Sun Nov 12 18:24:32 2023 -0300
    
            ejemplo listar y crear con views
    
        commit 5e340dfb978153a8f9d9039c0b1783fa888935dc
        Author: sink0la <ferreyra006@gmail.com>
        Date:   Sun Nov 12 16:57:01 2023 -0300
    
            creando views con clases
    
    commit 6d186f45ca52a12cce199887efa622bb108e5e06
    Author: Fantitom03 <leivafrancisco2017@gmail.com>
    Date:   Wed Nov 15 15:42:38 2023 -0300
    
        evaluaciones inicio
    
    commit 91962640954b25421bcfa57ccff712ae6def17e5
    Author: agustinhussein1 <agustinhusseindiaz@gmail.com>
    Date:   Mon Nov 13 20:17:36 2023 -0300
    
        (sigue siendo mi rama)
    
    commit 2f0b3407c3755a66e34fb0ec035907337ca11406
    Author: agustinhussein1 <agustinhusseindiaz@gmail.com>
    Date:   Mon Nov 13 20:17:17 2023 -0300
    
        cambios rama hussein (es mi rama)

--- apps/proyectotf/forms.py
+++ apps/proyectotf/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Proyecto_TF, Proyecto_TF_Alumno
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from datetime import datetime
 
 class Proyecto_TF_AlumnoForm(forms.ModelForm):
     class Meta:
@@ -12,4 +15,11 @@ class Proyecto_TF_Form(forms.ModelForm):
         model = Proyecto_TF
         fields = ('fecha_presentacion', 'director', 'co_director', 'asesor', 'titulo_ptf', 'descripcion', 'cstf', 'te_asignado')
 
+    def clean_fecha_presentacion(self):
+        fecha_presentacion = self.cleaned_data.get('fecha_presentacion')
+
+        if fecha_presentacion and fecha_presentacion < datetime.now().date():
+            raise ValidationError(_('La fecha de presentación no puede ser en el pasado.'))
+
+        return fecha_presentacion
 
